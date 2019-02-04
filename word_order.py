eebf220c7d1bf8a3b825a47d5a6c2253491395bb
r0gu3cic <39197273+r0gu3cic@users.noreply.github.com>
2019-02-05 00:51:47 +0100

    Create word_order.py

---
+++ word_order.py
@@ -0,0 +1,21 @@
+###INPUT
+while True:
+    x=int(input())#number of INPUTs
+    if x>1 and x<=100000:
+        break
+    else:
+        quit()
+words={}#collection of INPUTs, has do be dict because list research is too slow
+for i in range(x):
+    word=input()
+    if word not in words:
+        new_word={word:1}
+        words.update(new_word)
+    else:
+        words[word]+=1
+##OUTPUT
+print(str(len(words)))
+string=''
+for i in words.values():
+    string=string+str(i)+' '
+print(string)
