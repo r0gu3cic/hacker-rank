003132e010388408ec79517019985d6b83ee223f
r0gu3cic <39197273+r0gu3cic@users.noreply.github.com>
2019-02-05 00:17:08 +0100

    Create string_split_and_join.py

---
+++ string_split_and_join.py
@@ -0,0 +1,10 @@
+def split_and_join(line):
+    a=line
+    b=b=a.split()
+    separator='-'
+    result=separator.join(b)
+    return result
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
