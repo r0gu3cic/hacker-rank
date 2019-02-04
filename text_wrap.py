78fc54f9573935c8de8b0781294afb5eedcd940b
r0gu3cic <39197273+r0gu3cic@users.noreply.github.com>
2019-02-05 00:45:23 +0100

    Create text_wrap.py

---
+++ text_wrap.py
@@ -0,0 +1,11 @@
+def wrap(string, max_width):
+    list=textwrap.wrap(string,width=max_width)
+    text=''
+    for i in list:
+        text=text+i+'\n'
+    return text
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
