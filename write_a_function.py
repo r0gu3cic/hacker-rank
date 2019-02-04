14341dda4da1205aeaaf64a3cc032aa2f353bd0b
r0gu3cic <39197273+r0gu3cic@users.noreply.github.com>
2019-02-05 00:19:33 +0100

    Create write_a_function.py

---
+++ write_a_function.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    if year%4==0 and year%100!=0:
+        leap=True
+        return leap
+    elif year%400==0 and year%100==0:
+        leap=True
+        return leap
+    else:
+        leap=False
+        return leap
